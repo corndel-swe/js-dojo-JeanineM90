fc731023bb99dbf85e185f9cf28ddcd09d19d897
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-06-13 11:58:09 +0100

    JM needs to come back to this

--- functions/11.js
+++ functions/11.js
@@ -9,4 +9,18 @@
 export function adjustedSalary(salary, taxRate) {
   // TODO: apply the tax deduction
   // TODO: return the adjusted salary
+  return salary - (taxRate / 100) // this isn't complete- 1 pass and 4 failures 
 }
+
+/*
+multiple operators:
+We will need salary e.g 50000 to return the adjust salary of 45000 after being modified by the tax rate 
+The calculation of the tax rate is a percentage that needs minusing from the salary 
+We also need divide operator to / by 100 (percent)- So it's the tax rate calculation that is / 100  
+This will need to be done in two parts using ()
+*/
+/*
+The example solution is
+return salary * (1 - taxRate / 100)
+I am missing the 1- and the minus needs to be within the brackets- why? 
+*/
