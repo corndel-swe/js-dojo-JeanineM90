8dd248ef8c1ae0fb7bddf821d5ed7b56474e9e70
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-06-13 11:22:08 +0100

    07 with notes

--- functions/07.js
+++ functions/07.js
@@ -7,5 +7,11 @@
  * Example: didPass(75, 50) should return true.
  */
 export function didPass(score, passMark) {
-  // TODO: return true if score is greater than or equal to passMark
+  /* TODO: return true if score is greater than or equal to passMark
+  Need to compare teh score with the passMark
+  Is the score equal to or higher than the pass mark?
+  If so, it's true (return)
+  Similar to 06, we are using >=
+  */
+  return score >= passMark
 }
