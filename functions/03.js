5c39d736d3e4e96d048fa86f86745d15fe546d6c
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-06-13 11:00:55 +0100

    03 with notes

--- functions/03.js
+++ functions/03.js
@@ -7,5 +7,12 @@
  * Example: travelDistance(20, 15) should return 300.
  */
 export function travelDistance(fuelAmount, fuelEfficiency) {
-  // TODO: return the distance by multiplying fuelAmount and fuelEfficiency
+  /* TODO: return the distance by multiplying fuelAmount and fuelEfficiency
+  Similar to 01 & 02, it's going to be fuelAmount multiplied by fuelEfficiency (20 x 15 = 300)
+  The function is travelDistance. Can you return in one line rather than defining what the return should be:
+  */
+  return fuelAmount * fuelEfficiency
 }
+
+// previously, I would have written out const total then return total
+// but as the function stipulates the parameters already, we can just multiple these as the return 
