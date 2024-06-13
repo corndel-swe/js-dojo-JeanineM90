38cc36d8643c1b6f59aa469875fb73001dbe8648
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-06-13 14:56:32 +0100

    arr06

--- arrays/06.js
+++ arrays/06.js
@@ -9,4 +9,17 @@ export function shiftAllAges(ages) {
   // TODO: Use the shift() method to remove the first element
   // TODO: Use the push() method to add the removed element to the end of the array
   // TODO: Return the updated array
+
+  const first = ages.shift()
+  ages.push(first)
+  return ages
+
 }
+
+/*
+Have used shift in 04 and push in 03
+However, in this one, we are taking one value and adding it elsewhere, not removing 
+ages.shift() -- don't forget this will need const first value (needed example solution to check this/ needed prompt)
+ages.push(newage) -- this can't be new age as we're taking the first element and adding it back in, so should use const first 
+retuen the ages array
+*/
