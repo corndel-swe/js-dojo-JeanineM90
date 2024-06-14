01978eb9e3c1403d8978b4d777f2c1889b392ef5
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-06-14 11:19:50 +0100

    loops02 with notes

--- loops/02.js
+++ loops/02.js
@@ -13,4 +13,27 @@ export function productSmallerNumbers(input) {
   // TODO: Use a for loop to iterate from 1 to one less than the input number.
   // TODO: Multiply each number to the product variable.
   // TODO: Return the product.
+
+  let product = 1
+  for (let i = 1; i < input; i++) { // the counter; if i is lower than the input; add 1 to the counter and proceed
+    product *= i //  *= is x *= y or x = x * y 
+  }
+  return product
 }
+/*
+Product is basically the result 
+
+--
+
+For loops:
+for (do first: condition: do each loop) {} // this code get's executed before the loop begins, this loop continues as long as this condition is true 
+
+let i = 1 is the counter 
+The condition is is long as i < 6 the loop will continue
+After each loop has finished we want to increment i (make it bigger by 1 which is i++)
+
+for (let i = 1: i <6; i++) {
+console.log(i)
+}
+output is list of numbers 1-5 
+*/
