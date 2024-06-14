0fb35b4ce0978caa15320e0bf68887221b708794
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-06-14 12:05:00 +0100

    loops04 with faulty solution and comparison with correct solution

--- loops/04.js
+++ loops/04.js
@@ -14,4 +14,54 @@ export function sumOddNumbers(input) {
   // TODO: Check if the current number is odd.
   // TODO: If it is, add it to the sum variable.
   // TODO: Return the sum.
+
+  /* attempt 2:
+  let sum = 0
+  for (let i = 1; i < input; i++) {
+    if (i % 2 === 0) {
+      sum += i
+    }
+    else {
+      (sum % 2 != 0)
+    }
+  }
+  return sum
+*/
+
+  // 3rd attempt: 
+
+  let sum = 0
+  for (let i = 1; i < input; i++) {
+    if (i % 2 !== 0) { // else wasn't needed, !== simply used: "not equal value or not equal type"
+      sum += i
+    }
+  }
+  return sum
+
 }
+
+/*
+See 03 but need to change even operator to odd? 
+
+Remember filter() : filtering out items that don't match a certain condition e.g;
+const evenNumbers = numbers.filter(number => number % 2 === 0)
+"The statement number % 2 === 0 will return true if the number is even, and false if it's odd. 
+The filter() method will only include items in the new array if the statement returns true" (Tech docs)
+
+So can I use input.filter(i => i % 2 === 0)? Where? Do we need an else statement? 
+No: TypeError: input.filter is not a function 
+
+This is for arrays... 
+
+--
+
+2nd attempt: (sum % 2 != 0) as != is not equal too... simpler solution found by googling "find odd number in a loop"
+However:
+    1) should return 9 for input 6
+    ✔ should return 0 for input 1
+    2) should return 25 for input 10
+
+3rd Attempts: checked example solution 
+I was over complicating this initially, then as on the right track looking at the Comparison Operator, but added an extra step that wasn't needed
+
+*/
